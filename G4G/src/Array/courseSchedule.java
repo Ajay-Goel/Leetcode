6f9bdde50c0b711e4c70f72e0ff92e4ed9b87df7
Ajay-Goel <ajaygoel1993@gmail.com>
2020-02-22 23:52:45 -0500

    #14

--- G4G/src/Array/courseSchedule.java
+++ G4G/src/Array/courseSchedule.java
@@ -1,6 +1,8 @@
 package Array;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class courseSchedule {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
@@ -34,10 +36,48 @@ public class courseSchedule {
         return true;
     }
 
-    public static void main(){
+    boolean bfs(int courses, int[][]prereq){
+        ArrayList[] graph = new ArrayList[courses];
+        int count=0;
+        int [] degree = new int[courses];
+        Queue<Integer> q = new LinkedList<>();
+        for(int i=0;i<courses;i++)
+            graph[i]= new ArrayList();
+
+        for(int i=0;i<prereq.length;i++){
+            graph[prereq[i][0]].add(prereq[i][1]);
+            degree[prereq[i][1]]++;
+        }
+
+        for(int i=0;i<courses;i++){
+            if(degree[i]==0) {
+                q.add(i);
+                count++;
+            }
+        }
+
+        while(!q.isEmpty()){
+            int course = q.poll();
+            for(int i=0;i<graph[course].size();i++){
+                int pointer = (int)graph[course].get(i);
+                degree[pointer]--;
+                if(degree[pointer]==0){
+                    q.offer(pointer);
+                    count++;
+                }
+            }
+        }
+        if(count==courses)
+            return true;
+        return false;
+
+    }
+    public static void main(String args[]){
         courseSchedule cs = new courseSchedule();
         int [][] arr = {{1,0},{0,1}};
-        boolean res = cs.canFinish(2,arr);
+        boolean res = cs.canFinish(2,arr); //dfs
+        boolean res2 = cs.bfs(2,arr); //bfs
         System.out.println(res);
+        System.out.println(res2);
     }
 }
